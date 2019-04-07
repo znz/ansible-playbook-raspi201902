30619eca00f16a9a9ff7a3264c9fad4d7a0f9b9e
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2019-04-07 10:12:11 +0900

    Update slack.sh

--- provision/roles/zabbix-nasne/files/slack.sh
+++ provision/roles/zabbix-nasne/files/slack.sh
@@ -6,19 +6,21 @@ message="$(echo "$3" | tr -d '\r')"
 url="${4?usage: $0 to subject message url [username]}"
 username="${5:-Zabbix}"
 
-trigger_name="$(echo "$message" | sed -n 's/^Trigger: //p')"
 trigger_status="$(echo "$message" | sed -n 's/^Trigger status: //p')"
-trigger_severity="$(echo "$message" | sed -n 's/^Trigger severity: //p')"
 trigger_url="$(echo "$message" | sed -n 's/^Trigger URL: //p')"
 item1="$(echo "$message" | sed -n 's/^1\. //p')"
 item2="$(echo "$message" | sed -n 's/^2\. //p')"
 item3="$(echo "$message" | sed -n 's/^3\. //p')"
-event_id="$(echo "$message" | sed -n 's/^Original event ID: //p')"
 
-case "$trigger_status" in
-    OK)
+problem_name="$(echo "$message" | sed -n 's/^Problem name: //p')"
+host="$(echo "$message" | sed -n 's/^Host: //p')"
+severity="$(echo "$message" | sed -n 's/^Severity: //p')"
+event_id="$(echo "$message" | sed -n 's/^Original problem ID: //p')"
+
+case "$subject" in
+    Resolved:*)
 	emoji=':smile:'
-	case "$trigger_severity" in
+	case "$severity" in
 	    Information)
 		color="#439FE0"
 		;;
@@ -27,9 +29,9 @@ case "$trigger_status" in
 		;;
 	esac
 	;;
-    PROBLEM)
+    Problem:*)
 	emoji=':frowning:'
-	case "$trigger_severity" in
+	case "$severity" in
 	    Information)
 		color="#439FE0"
 		;;
@@ -52,19 +54,20 @@ item_to_json () {
     local item="$2"
     local misc="${3:-}"
     case "$item" in
-	*UNKNOWN*|'')
-	    ;;
-	*)
-	    echo "{\"title\":\"${title//\"/\\\"}\",\"value\":\"${item//\"/\\\"}\"$misc},"
-	    ;;
+        *UNKNOWN*|'')
+            ;;
+        *)
+            echo "{\"title\":\"${title//\"/\\\"}\",\"value\":\"${item//\"/\\\"}\"$misc},"
+            ;;
     esac
 }
 
 fields="\
-$(item_to_json 'Trigger' "${trigger_name}")\
-$(item_to_json 'Trigger severity' "${trigger_severity}" ',"short":true')\
+$(item_to_json 'Problem name' "${problem_name}" ',"short":true')\
+$(item_to_json 'Host' "${host}" ',"short":true')\
+$(item_to_json 'Severity' "${severity}" ',"short":true')\
 $(item_to_json 'Trigger status' "${trigger_status}" ',"short":true')\
-$(item_to_json 'Original event ID' "${event_id}" ',"short":true')\
+$(item_to_json 'Original problem ID' "${event_id}" ',"short":true')\
 $(item_to_json 'Trigger URL' "${trigger_url}")\
 $(item_to_json '1.' "$item1")\
 $(item_to_json '2.' "$item2")\
